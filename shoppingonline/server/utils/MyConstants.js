690dd84ff0430b238116e321fda8a052aaf333ee
Riocavano <134668091+Riocavano@users.noreply.github.com>
2023-07-14 11:02:40 +0700

    Add files via upload

---
+++ shoppingonline/server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'nguyen.ji37ie1.mongodb.net',
+  DB_USER: 'admin',
+  DB_PASS: '123',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'gianguyengien@gmail.com',
+  EMAIL_PASS: 'yvyisgtdyrmwjkgg',
+  JWT_SECRET: 'nguyenkk',
+  JWT_EXPIRES: '720000000', // in milliseconds
+};
+module.exports = MyConstants;
